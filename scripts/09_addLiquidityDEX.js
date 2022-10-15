0c5997a0c334621f562948426f6591afa997c353
nghiangovan <nghiangovan.bg@gmail.com>
2022-10-15 13:26:11 +0700

    change-script-add-liquidity

--- scripts/09_addLiquidityDEX.js
+++ scripts/09_addLiquidityDEX.js
@@ -1,5 +1,6 @@
 const hre = require("hardhat");
 const { ethers } = require("hardhat");
+const { WBNB } = require("../configs/wbnb");
 
 const { getAddressFromMapJson } = require("../utils/readWriteFileMapJson");
 
@@ -9,6 +10,14 @@ async function main() {
   let addressRouter = getAddressFromMapJson(chainId, "LabradoRouter02");
   let addressUSDC = getAddressFromMapJson(chainId, "USDC");
   let addressLabradoToken = getAddressFromMapJson(chainId, "LabradoToken");
+  let addressWBNB = WBNB[chainId];
+
+  let addressFactory = getAddressFromMapJson(chainId, "LabradoFactory");
+  const LabradoFactory = await ethers.getContractFactory(
+    "LabradoFactory",
+    deployer
+  );
+  const factory = await LabradoFactory.attach(addressFactory);
 
   const LabradoRouter02 = await ethers.getContractFactory(
     "LabradoRouter02",
@@ -37,6 +46,28 @@ async function main() {
   );
   await txApproveLBRD.wait(1);
 
+  let addressPairLBRD_USDC = await factory.getPair(
+    addressLabradoToken,
+    addressUSDC
+  );
+  const pairLBRD_USDC = await ethers.getContractFactory(
+    "LabradoPair",
+    deployer
+  );
+  const instancePairLBRD_USDC = await pairLBRD_USDC.attach(
+    addressPairLBRD_USDC
+  );
+  let isRouterLBRD_USDC = await instancePairLBRD_USDC.isRouter(addressRouter);
+  if (!isRouterLBRD_USDC) {
+    let txSetRouterLBRD_USDC = await factory.setMultiSetRoutersPair(
+      addressLabradoToken,
+      addressUSDC,
+      [addressRouter],
+      [true]
+    );
+    await txSetRouterLBRD_USDC.wait(1);
+    console.log("\nSet Router LBRD/USDC Done: ", txSetRouterLBRD_USDC.hash);
+  }
   let txAddLiquidLBRD_USDC = await router.addLiquidity(
     addressUSDC,
     addressLabradoToken,
@@ -50,6 +81,23 @@ async function main() {
   await txAddLiquidLBRD_USDC.wait(1);
   console.log("\nAdd Liquidity LBRD/USDC Done: ", txAddLiquidLBRD_USDC.hash);
 
+  let addressPairLBRD_BNB = await factory.getPair(
+    addressLabradoToken,
+    addressWBNB
+  );
+  const pairLBRD_BNB = await ethers.getContractFactory("LabradoPair", deployer);
+  const instancePairLBRD_BNB = await pairLBRD_BNB.attach(addressPairLBRD_BNB);
+  let isRouterLBRD_BNB = await instancePairLBRD_BNB.isRouter(addressRouter);
+  if (!isRouterLBRD_BNB) {
+    let txSetRouterLBRD_BNB = await factory.setMultiSetRoutersPair(
+      addressLabradoToken,
+      addressWBNB,
+      [addressRouter],
+      [true]
+    );
+    await txSetRouterLBRD_BNB.wait(1);
+    console.log("\nSet Router LBRD/BNB Done: ", txSetRouterLBRD_BNB.hash);
+  }
   let txAddLiquidLBRD_BNB = await router.addLiquidityETH(
     addressLabradoToken,
     "10000000000000000000000",
